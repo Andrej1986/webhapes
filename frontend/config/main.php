5d63b59e92df1348693488f230750c283c56ff6f
Andrej Soukup <soukup.andrej@gmail.com>
2018-07-30 08:47:43 +0200

    update

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -42,7 +42,7 @@ return [
 				'class' => 'Swift_SmtpTransport',
 				'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
 				'username' => 'srohapes@gmail.com',
-				'password' => 'hapesjazvecorol18',
+				'password' => '',
 				'port' => '587', // Port 25 is a very common port too
 				'encryption' => 'tls', // It is often used, check your provider or mail server specs
 				'streamOptions' => [
